4df6426f6e886ca6a9441873fe4eb6099489b542
RagamsettyBharathkumar-ai <ragamsettybharathkumar@gmail.com>
2026-04-28 17:09:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,12 @@ from datetime import datetime
 
 app = Flask(__name__)
 CORS(app)
-
+@app.route('/')
+def serve_index():
+    return send_from_directory('.', 'index.html')
+@app.route('/<path:path>')
+def serve_files(path):
+    return send_from_directory('.', path)
 
 # =========================
 # INIT DATABASE
